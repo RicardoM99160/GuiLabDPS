18812f81778c4ae18b7a40d35149f0e8e3636ed0
Spinal29 <ricardoespinal@hotmail.com>
2020-09-10 12:20:07 -0600

    crudAngular

--- guia04/crudAngular/src/app/models/alumno.ts
+++ guia04/crudAngular/src/app/models/alumno.ts
@@ -1,6 +1,9 @@
 export class Alumno {
-    id:number = 0;
-    name:string = '';
-    lastname:string = '';
-    age:number = 0;
+    id: number = 0;
+    name: string = '';
+    lastname: string = '';
+    age: number = 0;
+    direccion: string = '';
+    telefono: string = '';
+    correo: string = '';
 }
